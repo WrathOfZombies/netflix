60e1022df9e71b1b8f8a3be57d22f586ce1f7b08
Bhargav Krishna <wrathofzombies@Bhargavs-MacBook-Pro.local>
2020-11-30 11:29:40 -0800

    wip

--- app.js
+++ app.js
@@ -1,4 +1,45 @@
-import "./components/app.js";
+import { Component } from "./framework.js";
+import { data } from "./data.js";
 import "./components/movie.js";
+import { first } from "./_.js";
 
-export function render(data, host) {}
+class NetflixApp {
+  constructor(getState, updateState) {
+    this.getState = getState;
+    this.updateState = updateState;
+  }
+
+  onMount() {
+    this.updateState({ content: data.rows });
+  }
+
+  render() {
+    const { content = [] } = this.getState();
+
+    const movies = content
+      .map(
+        (row, index) =>
+          row.length === 1 ? 
+          `<bill-board movieId="${first(row)}" billboardId="${index}" />`
+          `<movie-card title=" ${title}" @titleclicked="onMovieTitleClicked"></movie-card>`
+      )
+      .join("");
+
+    return [
+      `<div class="gallery">
+        <h1 @click="onClick">Netflix</h1>
+        ${movies}
+      </div>`,
+      `
+h1 { color: red; }
+
+.gallery { 
+  width: 1280px;
+  margin: 0 auto;
+}
+      `,
+    ];
+  }
+}
+
+Component(NetflixApp);
